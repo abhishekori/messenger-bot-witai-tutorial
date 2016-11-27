92bfa58135fb54e2c92e3c32ac017982224e8d41
abhishek <abhikori1994@gmail.com>
2016-11-27 16:15:51 +0530

    few bugs

--- services/wit.js
+++ services/wit.js
@@ -20,96 +20,28 @@ var firstEntityValue = function (entities, entity) {
 
 
 var actions = {
-	say (sessionId, context, message, cb) {
-		// Bot testing mode, run cb() and return
-		if (require.main === module) {
-			cb()
-			return
-		}
-
-		console.log('WIT WANTS TO TALK TO:', context._fbid_)
-		console.log('WIT HAS SOMETHING TO SAY:', message)
-		console.log('WIT HAS A CONTEXT:', context)
-
-		if (checkURL(message)) {
-			FB.newMessage(context._fbid_, message, true)
-		} else {
-			FB.newMessage(context._fbid_, message)
-		}
-
-		
-		cb()
-		
+	send(request, response) {
+		const {sessionId, context, entities} = request;
+		const {text, quickreplies} = response;
+		return new Promise(function(resolve, reject) {
+			console.log('sending...', JSON.stringify(response));
+			return resolve();
+		});
+	},
+	getForecast({context, entities}) {
+		return new Promise(function(resolve, reject) {
+			var findItem = firstEntityValue(entities, 'findItem')
+			if (findItem) {
+				context.findItem = findItem; // we should call a weather API here
+				delete context.missingFindItem;
+			} else {
+				context.missingFindItem = true;
+				delete context.findItem;
+			}
+			return resolve(context);
+		});
 	},
 
-	merge(sessionId, context, entities, message, cb) {
-		// Reset the weather story
-		delete context.forecast
-
-		// Retrive the location entity and store it in the context field
-		var loc = firstEntityValue(entities, 'location')
-		if (loc) {
-			context.loc = loc
-		}
-
-		// Reset the cutepics story
-		delete context.pics
-
-		// Retrieve the category
-		var category = firstEntityValue(entities, 'category')
-		if (category) {
-			context.cat = category
-		}
-
-		// Retrieve the sentiment
-		var sentiment = firstEntityValue(entities, 'sentiment')
-		if (sentiment) {
-			context.ack = sentiment === 'positive' ? 'Glad your liked it!' : 'Aww, that sucks.'
-		} else {
-			delete context.ack
-		}
-
-		cb(context)
-	},
-
-	error(sessionId, context, error) {
-		console.log(error.message)
-	},
-
-	// list of functions Wit.ai can execute
-	['fetch-weather'](sessionId, context, cb) {
-		// Here we can place an API call to a weather service
-		// if (context.loc) {
-		// 	getWeather(context.loc)
-		// 		.then(function (forecast) {
-		// 			context.forecast = forecast || 'sunny'
-		// 		})
-		// 		.catch(function (err) {
-		// 			console.log(err)
-		// 		})
-		// }
-
-		context.forecast = 'Sunny'
-
-		cb(context)
-	},
-
-	['fetch-pics'](sessionId, context, cb) {
-		var wantedPics = allPics[context.cat || 'default']
-		context.pics = wantedPics[Math.floor(Math.random() * wantedPics.length)]
-
-		cb(context)
-	},locateItem(sessionId,context,entities,cb){
-		var findItem=firstEntityValue(entities,"findItem");
-		if(findItem){
-			//delete context.missingFindItem missingFindItem;
-		}else{
-			//context.missingFindItem = true;
-			//delete context.findItem;
-		}
-
-		 cb(context);
-	}
 }
 
 // SETUP THE WIT.AI SERVICE
